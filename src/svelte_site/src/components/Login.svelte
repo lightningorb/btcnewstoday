0228b6cfe8d26aa869a61c09f6c1758c644df990
admin <admin@example.com>
2022-11-09 10:31:32 +0800

    adding login

---
+++ src/svelte_site/src/components/Login.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import { InputGroup, InputGroupText, Input } from 'sveltestrap';
+</script>
+
+<h1>Login</h1>
+
+<InputGroup>
+<table width='100px'>
+  <tr>
+    <td>
+      <InputGroupText>username</InputGroupText>
+    </td>
+    <td>
+      <Input placeholder="username" />
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <InputGroupText>password</InputGroupText>
+    </td>
+    <td>
+      <Input placeholder="password" />
+    </td>
+  </tr>
+</table>
+</InputGroup>
+
+<br />
+
+<button>Log in</button>
+
+
+<h1>Register</h1>
+
+<InputGroup>
+<table width='100px'>
+  <tr>
+    <td>
+      <InputGroupText>username</InputGroupText>
+    </td>
+    <td>
+      <Input placeholder="username" />
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <InputGroupText>password</InputGroupText>
+    </td>
+    <td>
+      <Input placeholder="password" />
+    </td>
+  </tr>
+</table>
+</InputGroup>
+
+<br />
+
+<button>Register</button>
+
+<br />
+<br />
--- src/svelte_site/src/routes/settings/+page.svelte
+++ src/svelte_site/src/routes/settings/+page.svelte
@@ -6,4 +6,6 @@
 
 <h1>Settings</h1>
 
+<a href='/login'>log in</a>
+
 <p>This is the settings page.</p>
