42ac9c1adaaec70bf831ac50116de20ce407a3d6
orb <admin@lnorb.com>
2022-11-29 09:28:58 +0800

    remove dashboard link [issue #8]

--- src/svelte_site/src/components/Nav.svelte
+++ src/svelte_site/src/components/Nav.svelte
@@ -7,5 +7,4 @@
   <a href="/events" data-sveltekit-prefetch>Events</a>
   <a href="/podcasts" data-sveltekit-prefetch>Podcasts</a>
   <a href="/jobs" data-sveltekit-prefetch>Jobs</a>
-  <a href="/dashboard" data-sveltekit-prefetch>Dashboard</a>
 </nav>
