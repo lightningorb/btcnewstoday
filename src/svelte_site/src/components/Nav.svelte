5ec32a114758b046ce9843735921dabdb14f1500
orb <admin@lnorb.com>
2022-11-29 12:02:31 +0800

    center header and footer [issue #7]

--- src/svelte_site/src/components/Nav.svelte
+++ src/svelte_site/src/components/Nav.svelte
@@ -3,15 +3,54 @@
 </script>
 
 <nav>
-  <a href="/" data-sveltekit-prefetch>Home</a>
-  <a href="/latest" data-sveltekit-prefetch>Latest</a>
-  <a href="/longform" data-sveltekit-prefetch>Longform</a>
-  <a href="/past" data-sveltekit-prefetch>Past</a>
-  <a href="/about" data-sveltekit-prefetch>About</a>
-  <a href="/events" data-sveltekit-prefetch>Events</a>
-  <a href="/podcasts" data-sveltekit-prefetch>Podcasts</a>
-  <a href="/jobs" data-sveltekit-prefetch>Jobs</a>
-{#if $preferences.access_token != ''}
-  <a href="/dashboard" data-sveltekit-prefetch>Dashboard</a>
-{/if}
+  <ul class="nav justify-content-center">
+  <li class='nav-item'>
+    <a class='nav-link' href="/" data-sveltekit-prefetch>Home</a>
+  </li>
+  <li class='nav-item'>
+    <a class='nav-link' href="/latest" data-sveltekit-prefetch>Latest</a>
+  </li>
+  <li class='nav-item'>
+    <a class='nav-link' href="/longform" data-sveltekit-prefetch>Longform</a>
+  </li>
+  <li class='nav-item'>
+    <a class='nav-link' href="/past" data-sveltekit-prefetch>Past</a>
+  </li>
+  <li class='nav-item'>
+    <a class='nav-link' href="/about" data-sveltekit-prefetch>About</a>
+  </li>
+  <li class='nav-item'>
+    <a class='nav-link' href="/events" data-sveltekit-prefetch>Events</a>
+  </li>
+  <li class='nav-item'>
+    <a class='nav-link' href="/podcasts" data-sveltekit-prefetch>Podcasts</a>
+  </li>
+  <li class='nav-item'>
+    <a class='nav-link' href="/jobs" data-sveltekit-prefetch>Jobs</a>
+  </li>
+  {#if $preferences.access_token != ''}
+    <li class='nav-item'>
+      <a href="/dashboard" data-sveltekit-prefetch>Dashboard</a>
+    </li>
+  {/if}
+  </ul>
 </nav>
+
+<!-- 
+<nav>
+ <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
+</nav>
+ -->
