422a7395205ed686dd116b8daa684cc07941acea
orb <admin@lnorb.com>
2022-11-30 04:00:48 +0800

    nicer css [issue #3]

--- src/svelte_site/src/components/Article/Tweets.svelte
+++ src/svelte_site/src/components/Article/Tweets.svelte
@@ -5,7 +5,11 @@
   article.tweets.sort((a, b) => a.id - b.id);
 </script>
 
-<div style='padding-bottom: 10px;'><b>Tweets:</b>
+<div>
+
+{#if article.tweets.length > 0}
+  <b>Tweets:</b>
+{/if}
 
 {#each article.tweets as tweet}
   <Popover
@@ -16,7 +20,7 @@
   >
     {tweet.text}
   </Popover>
-  <a id={'id-'+tweet.id} target='_blank' href={`https://twitter.com/${tweet.username}/status/${tweet.id}`}>@{tweet.username}</a> <span style='width: 3px;'></span>
+  <a class='tweet' id={'id-'+tweet.id} target='_blank' href={`https://twitter.com/${tweet.username}/status/${tweet.id}`}>@{tweet.username}</a> <span style='width: 3px;'></span>
 {/each}
 
 </div>
--- src/svelte_site/src/components/Nav.svelte
+++ src/svelte_site/src/components/Nav.svelte
@@ -1,56 +1,44 @@
 <script lang="ts">
   import { preferences } from '$lib/store.js';
+  import { Col, Container, Row } from 'sveltestrap';
 </script>
 
-<nav>
-  <ul class="nav justify-content-center">
-  <li class='nav-item'>
-    <a class='nav-link' href="/" data-sveltekit-prefetch>Home</a>
-  </li>
-  <li class='nav-item'>
-    <a class='nav-link' href="/latest" data-sveltekit-prefetch>Latest</a>
-  </li>
-  <li class='nav-item'>
-    <a class='nav-link' href="/longform" data-sveltekit-prefetch>Longform</a>
-  </li>
-  <li class='nav-item'>
-    <a class='nav-link' href="/past" data-sveltekit-prefetch>Past</a>
-  </li>
-  <li class='nav-item'>
-    <a class='nav-link' href="/about" data-sveltekit-prefetch>About</a>
-  </li>
-  <li class='nav-item'>
-    <a class='nav-link' href="/events" data-sveltekit-prefetch>Events</a>
-  </li>
-  <li class='nav-item'>
-    <a class='nav-link' href="/podcasts" data-sveltekit-prefetch>Podcasts</a>
-  </li>
-  <li class='nav-item'>
-    <a class='nav-link' href="/jobs" data-sveltekit-prefetch>Jobs</a>
-  </li>
-  {#if $preferences.access_token != ''}
-    <li class='nav-item'>
-      <a href="/dashboard" data-sveltekit-prefetch>Dashboard</a>
-    </li>
-  {/if}
-  </ul>
-</nav>
-
-<!-- 
-<nav>
- <ul class="nav justify-content-center">
-  <li class="nav-item">
-    <a class="nav-link" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-  </li>
-</ul>
-</nav>
- -->
+<Container>
+  <Row cols={1}>
+    <Col>
+      <nav>
+        <ul class="nav justify-content-center">
+        <li class='nav-item'>
+          <a class='nav-link' href="/" data-sveltekit-prefetch>Home</a>
+        </li>
+        <li class='nav-item'>
+          <a class='nav-link' href="/latest" data-sveltekit-prefetch>Latest</a>
+        </li>
+        <li class='nav-item'>
+          <a class='nav-link' href="/longform" data-sveltekit-prefetch>Longform</a>
+        </li>
+        <li class='nav-item'>
+          <a class='nav-link' href="/past" data-sveltekit-prefetch>Past</a>
+        </li>
+        <li class='nav-item'>
+          <a class='nav-link' href="/about" data-sveltekit-prefetch>About</a>
+        </li>
+        <li class='nav-item'>
+          <a class='nav-link' href="/events" data-sveltekit-prefetch>Events</a>
+        </li>
+        <li class='nav-item'>
+          <a class='nav-link' href="/podcasts" data-sveltekit-prefetch>Podcasts</a>
+        </li>
+        <li class='nav-item'>
+          <a class='nav-link' href="/jobs" data-sveltekit-prefetch>Jobs</a>
+        </li>
+        {#if $preferences.access_token != ''}
+          <li class='nav-item'>
+            <a href="/dashboard" data-sveltekit-prefetch>Dashboard</a>
+          </li>
+        {/if}
+        </ul>
+      </nav>
+    </Col>
+  </Row>
+</Container>
