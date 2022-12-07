cfede1a8700484cbbcebe2aa11ec1695abc5fc76
admin <admin@example.com>
2022-12-08 06:25:07 +0800

    aesthetic changes

--- src/svelte_site/src/components/Podcast.svelte
+++ src/svelte_site/src/components/Podcast.svelte
@@ -5,6 +5,7 @@
   import {API_FQDN} from '$lib/constants.js';
   import axios from 'axios';
   export let podcast;
+  import Play from './Article/Play.svelte';
   let edit = false;
 
   function _delete(){
@@ -45,7 +46,7 @@
       <td><Input type="text" label="Name" bind:value={podcast.outlet}/></td>
       <td><Input type="text" label="Episode" bind:value={podcast.episode_title}/></td>
     {:else}
-      <td><a target='_blank' href='{podcast.link}'>{podcast.outlet}</a></td>
+      <td><Play podcast={podcast}/><a target='_blank' href='{podcast.link}'>{podcast.outlet}</a></td>
       <td>
         {podcast.episode_title}
         {#if $preferences.access_token != ''}
